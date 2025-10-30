be1dfcfce0a0bcdcf4859260177aa53ffe9c7400
rubenanko <rubenanko@gmail.com>
2025-10-30 16:41:34 +0100

    feat(): 2d exploration, cleaner transitions, enhanced customization, boundaries verifications, axis centering

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,76 @@ import scenes from "./scenes.json"
 
 function App() 
 {
+  const sceneRange = 20000;
+  const blurRange = sceneRange/100
+
   const [scrollXPosition, setScrollXPosition] = useState(0);
   const [scrollYPosition, setScrollYPosition] = useState(0);
+  const [indexX, setIndexX] = useState(0);
+  const [indexY, setIndexY] = useState(0);
+  const [directionX, setDirectionX] = useState(0);
+  const [directionY, setDirectionY] = useState(0);
 
   const handleScroll = (event) => {
-      const newScrollXPosition = Math.max(0,scrollXPosition + event.deltaX);
-      const newScrollYPosition = Math.max(0,scrollYPosition + event.deltaY);
+      const newScrollXPosition = Math.min(Math.max(0,scrollXPosition + event.deltaX),(scenes.length-1)*sceneRange*5/4 - 1);
+      const newScrollYPosition = Math.min(Math.max(0,scrollYPosition + event.deltaY),(scenes[indexX].length-1)*sceneRange*5/4 - 1);
       setScrollXPosition(newScrollXPosition);
       setScrollYPosition(newScrollYPosition);
   };
 
-  const sceneRange = 10000;
-  let index = Math.round(scrollYPosition/sceneRange)
-  let blur = Math.abs((index*sceneRange - scrollYPosition)/100)
-  let volume = 1 - blur/50
-  let preambule = ""
+  // save the amount of re renders
+
+  let tmpIndexY = Math.round(scrollYPosition/sceneRange)
+  let tmpIndexX = Math.round(scrollXPosition/sceneRange)
+
+  // update the index, and coming direction
+  if(tmpIndexX != indexX)
+  {
+    setIndexX(tmpIndexX)
+    setDirectionX(tmpIndexX*sceneRange - scrollXPosition)
+    setDirectionY(0)
+  }
+
+  if(tmpIndexY != indexY)
+  {
+    setIndexY(tmpIndexY)
+    setDirectionY(tmpIndexY*sceneRange - scrollYPosition)
+    setDirectionX(0)
+  }
+
+
+  // recenter the scrolling on the current main axis
+  if((indexX*sceneRange != scrollXPosition) && Math.abs(indexX*sceneRange - scrollXPosition)/Math.abs(indexY*sceneRange - scrollYPosition) < 0.5)
+    setScrollXPosition(indexX*sceneRange)
+
+  else
+  {
+    if((indexY*sceneRange != scrollYPosition) && Math.abs(indexY*sceneRange - scrollYPosition)/Math.abs(indexX*sceneRange - scrollXPosition) < 0.5)
+      setScrollYPosition(indexY*sceneRange)
+  }
+
+  // calculate blur values
+  let distance = Math.sqrt(Math.pow(indexY*sceneRange - scrollYPosition,2) + Math.pow(indexX*sceneRange - scrollXPosition,2))
+  let blur = distance/blurRange
+  let volume = Math.exp(-blur/20)
+
+  // set up the text
+  let text = null
+  if((indexY*sceneRange - scrollYPosition)*directionY > 0 || (indexX*sceneRange-scrollXPosition)*directionX > 0)
+    text = <>
+              <h2 className="text-9xl" style={{color:`#${scenes[indexX][indexY].textColor}`,fontFamily: scenes[indexX][indexY].font}}>{scenes[indexX][indexY].title}</h2>
+              <p className="text-5xl" style={{color:`#${scenes[indexX][indexY].textColor}`,fontFamily: scenes[indexX][indexY].font}}>{scenes[indexX][indexY].Director}</p>
+              <p className="text-3xl" style={{color:`#${scenes[indexX][indexY].textColor}`,fontFamily: scenes[indexX][indexY].font}}>{scenes[indexX][indexY].Year}</p>
+          </>
+  else
+    text = <p className="text-5xl" style={{color:`#${scenes[indexX][indexY].textColor}`,fontFamily: scenes[indexX][indexY].font}}>{scenes[indexX][indexY].preambule}</p>
 
   return (
-    <div onWheel={() => {handleScroll(event);console.log(blur)}}>
-      <Player src={scenes[index]}
+    <div onWheel={() => {handleScroll(event)}}>
+      <Player src={scenes[indexX][indexY].video}
               blur={blur}
               volume={volume}
-              preambule={preambule}/>
+              text={text}/>
     </div>
   )
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,17 @@
 import ReactPlayer from 'react-player'
 import { useState } from 'react'
 
-function Player({src,blur,volume,preambule})
+function Player({src,blur,volume,text})
 {
     const [play,setPlay] = useState(false);
+    // const textBlur = Math.abs(Math.pow((Math.abs(blur/50-0.5))*2,0.2)-1)
+    let textBlur = Math.exp(-Math.pow((blur/50-0.5)*2,4)*10)
+    if(textBlur < 0.01)
+        textBlur = 0
 
     return (
         <div className='absolute w-full h-full top-0 left-0'>
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-5xl z-10" style={{blur:`blur(${blur}px)`,opacity:blur/90}}>{preambule}</div>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10" style={{opacity:textBlur}}>{text}</div>
             <div className='absolute top-0 left-0 w-full h-full' style={{backdropFilter:`blur(${blur}px)`}}></div>
             <div className='absolute top-0 left-0 w-full h-full bg-black' style={{opacity:blur/100}}></div>
             <ReactPlayer
