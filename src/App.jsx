44e3df6b146cb6b55ddeda2075ad63e12ea5c676
rubenanko <rubenanko@gmail.com>
2025-11-24 17:00:10 +0100

    feat(): horizontal navigation

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,13 @@ function App()
   const [showTitle,setShowTitle] = useState(false);
   const [indexX,setIndexX] = useState(0);
   const [indexY,setIndexY] = useState(0);
+  const [isNavigationVertical,setIsNavigationVertical] = useState(true);
 
+  const handleNavigationBarClick = () => {
+    setIsNavigationVertical(!isNavigationVertical)
+  }
+
+  // callback function on scroll
   const handleScroll = (event) => {
     if(!coolDown)
       if(event.deltaY != 0)
@@ -26,11 +32,15 @@ function App()
         else
           if (nextState < 0)
             nextState = 3
-          
 
         setSceneState(nextState);
 
-        if(Math.sign(event.deltaY) == directionY)
+        let direction = isNavigationVertical ? directionY : directionX
+        let setDirection = isNavigationVertical ? setDirectionY : setDirectionX
+        let index = isNavigationVertical ? indexY : indexX
+        let setIndex = isNavigationVertical ? setIndexY : setIndexX
+
+        if(Math.sign(event.deltaY) == direction)
           setShowTitle(false)
         else
           setShowTitle(true)
@@ -38,15 +48,16 @@ function App()
         if(nextState == STATE.TRANSITION)
         {
           setTimeout(() => {
-            let newIndex = indexY+Math.sign(event.deltaY);
-            if(newIndex >= scenes[0].length)
+            let newIndex = index+Math.sign(event.deltaY);
+            let length = isNavigationVertical ? scenes[indexX].length : scenes.length
+            if(newIndex >= length)
               newIndex = 0;
             if(newIndex < 0)
               newIndex = scenes[0].length-1;
-            setIndexY(newIndex)
+            setIndex(newIndex)
             },1000)
           setTimeout(() => {setSceneState(Math.abs((nextState+ Math.sign(event.deltaY)) % 4))},1000);
-          setDirectionY(Math.sign(event.deltaY))
+          setDirection(Math.sign(event.deltaY))
           setShowTitle(true);
         }
 
@@ -68,8 +79,10 @@ function App()
     else
         text = <p className="text-5xl" style={{color:`#${scenes[indexX][indexY].textColor}`,fontFamily: scenes[indexX][indexY].font}}>{scenes[indexX][indexY].preambule}</p>
   
+    
+    // handle blur
   let blur = blurValue;
-
+  
   if(sceneState == STATE.VIDEO)
     blur = 0
   else
@@ -77,8 +90,28 @@ function App()
       blur = 100
 
 
+
+  // handle the navigation direction change
+
+  let navigationBarStyle = null
+  if(sceneState == STATE.VIDEO)
+    if(isNavigationVertical)
+      navigationBarStyle = 'rotate-90 absolute z-10 left-[50px] bottom-[50px] text-red-500 opacity-0 hover:opacity-255 transition-all duration-300'
+    else
+      navigationBarStyle = 'absolute z-10 left-[50px] bottom-[50px] text-red-500 opacity-0 hover:opacity-255 transition-all duration-300'
+  else
+    navigationBarStyle = 'hidden absolute z-10 left-[50px] bottom-[50px] text-red-500 opacity-0 hover:opacity-255 transition-all duration-300'
+
+
   return (
     <div onWheel={() => {handleScroll(event);}}>
+      <div className={navigationBarStyle} onClick={handleNavigationBarClick}>
+        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+          width="200px" height="200px" viewBox="0 0 290.658 290.658"
+          xml:space="preserve">
+          <g><g><rect y="139.474" style={{"fill":"currentColor"}} width="290.658" height="11.711"/></g></g>
+        </svg>
+      </div>
       <Player src={scenes[indexX][indexY].video}
               blur={blur}
               volume={0}
